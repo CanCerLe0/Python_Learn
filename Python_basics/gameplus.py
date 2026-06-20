1c96fae2bd2a6f8773f4030b0e7b7391ebfde9aa
CanCerLe0 <liu1940893092@126.com>
2026-06-20 13:40:44 +0800

    猜数字循环版

---
+++ Python_basics/gameplus.py
@@ -0,0 +1,16 @@
+"""用python设计一个游戏"""
+
+temp = input("猜一个我心里想的数字：") #input函数的作用是获取用户输入的内容，返回一个字符串
+guess = int(temp) #int函数的作用是将字符串转换为整数，如果输入的内容不能转换为整数，会抛出一个异常
+
+while guess != 8: #while语句的使用同C语言，注意要有冒号
+    if guess < 8:
+        print("你猜的数字小了！")
+    elif guess > 8:
+        print("你猜的数字大了！")
+    elif guess ==8:
+        break #break作用同C语言，跳出循环
+    temp = input("请重新输入：")
+    guess = int(temp)
+
+print("你猜对了！")
