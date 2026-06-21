d0b822c22bd3d2bb7d4c33eda6315731e331388b
CanCerLe0 <liu1940893092@126.com>
2026-06-21 16:40:43 +0800

    分支结构

---
+++ Python_basics/branch.py
@@ -0,0 +1,48 @@
+"""
+Python分支结构
+"""
+
+#if elif else同C语言
+status_code = int(input('响应状态码: '))
+if status_code == 400:
+    description = 'Bad Request'
+elif status_code == 401:
+    description = 'Unauthorized'
+elif status_code == 403:
+    description = 'Forbidden'
+elif status_code == 404:
+    description = 'Not Found'
+elif status_code == 405:
+    description = 'Method Not Allowed'
+elif status_code == 418:
+    description = 'I am a teapot'
+elif status_code == 429:
+    description = 'Too many requests'
+else:
+    description = 'Unknown status Code'
+print('状态码描述:', description)
+
+#match-case结构是Python3.10引入的结构化模式匹配语句
+#match-case结构的语法与switch-case结构类似 但功能更强大 可以匹配复杂的数据结构
+status_code1 = int(input('响应状态码: '))
+match status_code1:
+    case 400: description = 'Bad Request'
+    case 401: description = 'Unauthorized'
+    case 403: description = 'Forbidden'
+    case 404: description = 'Not Found'
+    case 405: description = 'Method Not Allowed'
+    case 418: description = 'I am a teapot'
+    case 429: description = 'Too many requests'
+    #带有`_`的`case`语句在代码中起到通配符的作用，如果前面的分支都没有匹配上，代码就会来到`case _`
+    case _: description = 'Unknown Status Code' 
+print('状态码描述:', description)
+
+#将多个代码对应同一个分支时 可以使用|符号连接多个case语句
+status_code2 = int(input('响应状态码: '))
+match status_code2:
+    case 400 | 405: description = 'Invalid Request'
+    case 401 | 403 | 404: description = 'Not Allowed'
+    case 418: description = 'I am a teapot'
+    case 429: description = 'Too many requests'
+    case _: description = 'Unknown Status Code'
+print('状态码描述:', description)
